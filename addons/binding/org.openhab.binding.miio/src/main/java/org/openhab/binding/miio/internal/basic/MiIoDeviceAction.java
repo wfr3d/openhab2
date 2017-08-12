61f8775192e55485abaf5e7f55cf7ae3202d73eb
Marcel Verpaalen <marcel@verpaalen.com>
2017-08-13 01:24:28 +0200

    [miio] add basic functionality to control other miio devices
    * Generate channels based on database
    * Update properties based on database
    * Execute commands based on database
    
    Signed-off-by: Marcel Verpaalen marcel@verpaalen.com

---
+++ addons/binding/org.openhab.binding.miio/src/main/java/org/openhab/binding/miio/internal/basic/MiIoDeviceAction.java
@@ -0,0 +1,53 @@
+/**
+ * Copyright (c) 2010-2017 by the respective copyright holders.
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.openhab.binding.miio.internal.basic;
+
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
+/*  Mapping actions from json
+*
+* @author Marcel Verpaalen - Initial contribution
+*/
+public class MiIoDeviceAction {
+
+    @SerializedName("command")
+    @Expose
+    private String command;
+    @SerializedName("channel")
+    @Expose
+    private String channel;
+    @SerializedName("type")
+    @Expose
+    private String type;
+
+    public String getCommand() {
+        return command;
+    }
+
+    public void setCommand(String command) {
+        this.command = command;
+    }
+
+    public String getChannel() {
+        return channel;
+    }
+
+    public void setChannel(String channel) {
+        this.channel = channel;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+}
