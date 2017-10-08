618e0e0389d81f004a4769e0acfacb4525c6e027
Marcel Verpaalen <marcel@verpaalen.com>
2017-10-08 11:40:49 +0200

    [miio] add transformation option to db
    * updated airpurifier v6 & m1 to use transformation
    * Fix typo in channel name filtermaxLife
    
    Signed-off-by: Marcel Verpaalen <marcel@verpaalen.com>

---
+++ addons/binding/org.openhab.binding.miio/src/main/java/org/openhab/binding/miio/internal/basic/Conversions.java
@@ -0,0 +1,42 @@
+/**
+ * Copyright (c) 2010-2017 by the respective copyright holders.
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.openhab.binding.miio.internal.basic;
+
+import java.util.concurrent.TimeUnit;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.google.gson.JsonElement;
+import com.google.gson.JsonPrimitive;
+
+/**
+ * Conversion for values
+ *
+ * @author Marcel Verpaalen - Initial contribution
+ */
+public class Conversions {
+    private final static Logger logger = LoggerFactory.getLogger(Conversions.class);
+
+    public static JsonElement SecondsToHours(JsonElement seconds) {
+        long hours = TimeUnit.SECONDS.toHours(seconds.getAsInt());
+        return new JsonPrimitive(hours);
+    }
+
+    public static JsonElement execute(String transfortmation, JsonElement value) {
+        switch (transfortmation.toUpperCase()) {
+            case "SECONDSTOHOURS":
+                return SecondsToHours(value);
+            default:
+                logger.debug("Transformation {} not found. Returning '{}'", transfortmation, value.toString());
+                return value;
+        }
+    }
+
+}
